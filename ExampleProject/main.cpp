6d6454ec971d3522559dac5938eb1d471523db6b
Br00W <Smoke.Fomin@yandex.ru>
2026-09-05 20:17:42 +0300

    added dev-branch and ended programm with counter of area

--- ExampleProject/main.cpp
+++ ExampleProject/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello world" << std::endl;
+
+	float side, height, area;
+
+	std::cout << "Welcome to calculator of the area of a triangle!" << std::endl;
+	std::cout << "Enter the side: " << std::endl;
+	std::cin >> side;
+	std::cout << "Enter the height corresponding to the given side: " << std::endl;
+	std::cin >> height;
+
+	area = 0.5 * side * height;
+
+	std::cout << "The area of triangle is " << area;
+
 	return 0;
 }
